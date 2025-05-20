99b3f4df59e294ffc4df09c3d96fa6eb372ccdab
maiarasalmaso <91421583+maiarasalmaso@users.noreply.github.com>
2025-05-19 21:14:43 -0300

    Update temperatira srk.py

--- temperatira srk.py
+++ temperatira srk.py
@@ -1,3 +1,3 @@
 import CoolProp.CoolProp as CP
-Temperatura = CP.PropsSI("T","P",101325,"Q",0,"SRK::Propane")
+Temperatura = CP.PropsSI("T","P",101325,"Q",0,"SRK::Propane") # temperatura que eu preciso, pressão e título fornecido, modelo termodinamico, componente 
 print (Temperatura)
